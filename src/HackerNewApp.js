7c4d0d67f44ae041034aab64da07a5603f37ffb9
luzmar7 <luzmar7.luz@gmail.com>
2022-03-23 13:47:10 -0400

    iniciando prueba Reign, componente Head

---
+++ src/HackerNewApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from './components/Head'
+
+const HackerNewApp = () => {
+  return (
+    <>
+      <Head />      
+    </>
+  )
+}
+
+export default HackerNewApp
---
+++ src/components/Head.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Head.css';
+
+const Head = ({ title }) => {
+  return (
+<div className="Rectangle-2-Copy">
+  <span >
+    HACKER NEWS
+  </span>
+</div>
+  )
+}
+
+
+export default Head
